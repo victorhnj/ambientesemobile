697d16ee3c28e3055dc947bdcf9b29ed6ccb8f16
victorhnj <victor.goioere@hotmail.com>
2024-10-29 21:32:52 -0300

    Tela Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
-// Certifique-se de que o caminho está correto // Importa a nova tela de recuperação de senha
+import 'login_screen.dart'; // Importa a tela de login
+import 'tela_inicial.dart'; // Importa a nova tela de perguntas
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(), // A tela inicial será a de login
+      initialRoute:
+          '/tela_inicial', // Define a rota inicial como a tela inicial
+      routes: {
+        '/': (context) => LoginScreen(), // Rota da tela de login
+        '/tela_inicial': (context) =>
+            TelaInicial(), // Rota para a nova tela de perguntas
+      },
     );
   }
 }
--- lib/tela_inicial.dart
+++ lib/tela_inicial.dart
@@ -38,38 +38,55 @@ class TelaInicial extends StatelessWidget {
           children: [
             const CircleAvatar(
               radius: 50,
-              child: Icon(Icons.person, size: 60), // Ícone de pessoa
+              child: Icon(Icons.person,
+                  size: 60, color: Colors.black), // Ícone de pessoa em preto
             ),
             const SizedBox(height: 16),
             const Text(
               'Bem vindo!',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black), // Texto em preto
             ),
             const SizedBox(height: 8),
             const Text(
               'Conheça algumas das principais opções que você pode realizar...',
               textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black), // Texto em preto
             ),
             const SizedBox(height: 32),
             ElevatedButton.icon(
-              icon: const Icon(Icons.edit),
-              label: const Text('Realizar Avaliação'),
+              icon:
+                  const Icon(Icons.edit, color: Colors.black), // Ícone em preto
+              label: const Text(
+                'Realizar Avaliação',
+                style: TextStyle(color: Colors.black), // Texto em preto
+              ),
               onPressed: () {
                 // Navegar para a tela de avaliação
               },
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size(double.infinity, 50),
+                backgroundColor: Colors.white, // Fundo branco para o botão
+                side: BorderSide(color: Colors.black), // Borda preta
               ),
             ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
-              icon: const Icon(Icons.person_add),
-              label: const Text('Cadastrar Empresa'),
+              icon: const Icon(Icons.person_add,
+                  color: Colors.black), // Ícone em preto
+              label: const Text(
+                'Cadastrar Empresa',
+                style: TextStyle(color: Colors.black), // Texto em preto
+              ),
               onPressed: () {
                 // Navegar para a tela de cadastro de empresa
               },
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size(double.infinity, 50),
+                backgroundColor: Colors.white, // Fundo branco para o botão
+                side: BorderSide(color: Colors.black), // Borda preta
               ),
             ),
           ],
